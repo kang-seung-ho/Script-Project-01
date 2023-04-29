6a8f77629903efd19a488915fa8d82ccc52b4ef5
강승호 <112061429+kang-seung-ho@users.noreply.github.com>
2023-04-30 00:02:55 +0900

    최종

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 from customtkinter import *
 from customtkinter import CTk as Tk
 from customtkinter import CTkLabel as Label
-from customtkinter import CTkFrame as Frame
 from customtkinter import CTkButton as Button
-from customtkinter import CTkEntry as Entry
-from customtkinter import CTkTextbox as ScrolledText
-from customtkinter import CTkCheckBox as Checkbutton
 from customtkinter import CTkRadioButton as Radiobutton
 import tkinter.messagebox as messagebox
 import shutil
 from PIL import Image
 import datetime
 import zipfile
-# from fpdf import FPDF
 
 window = Tk()
 window.title('File Assistant')
@@ -22,9 +17,6 @@ window.resizable(False, False)
 #선택된 폴더 위치
 selectedFolerpath=''
 
-# #보이지 않는 액자
-# first_line_frame=Frame(window)
-# first_line_frame.pack()
 
 def getPhotoDate(filepath):
     try:
@@ -33,13 +25,11 @@ def getPhotoDate(filepath):
             exifdata = image._getexif()
             if exifdata:
                 for tag, value in exifdata.items():
-                    if tag == 36867: # DateTimeOriginal tag
+                    if tag == 36867: # 날짜태그
                         return datetime.datetime.strptime(value, '%Y:%m:%d %H:%M:%S')
     except Exception as errorN:
         messagebox.showerror('에러', '날짜를 불러오지 못한 사진이 있습니다.')
     return None
-
-
 
 
 def sortPictures():
@@ -48,8 +38,7 @@ def sortPictures():
     if selectedFolerpath == '':
         messagebox.showerror('에러', '폴더를 먼저 지정하십시오')
         return
-
-    # create folders for each year and month
+    #사진이 있는 날에 대해서만 연, 월 폴더 추가
     for filename in os.listdir(selectedFolerpath):
         if filename.lower().endswith(('.jpg', '.jpeg', '.png', '.gif', '.bmp')):
             filepath = os.path.join(selectedFolerpath, filename)
@@ -62,9 +51,6 @@ def sortPictures():
                 if not os.path.exists(month_folder):
                     os.makedirs(month_folder)
                 shutil.move(filepath, month_folder)
-                # 현재 처리중인 파일 진행상황 출력
-                # nowpathlabel = Label(window, text='처리중 경로: ' + filepath)
-                # nowpathlabel.place(x=0, y=80)
 
     #완료되었다는 팝업창 띄우기
     messagebox.showinfo('완료 안내', '사진 정리가 끝났습니다.')
@@ -95,144 +81,119 @@ selectFolButton = Button(window, text='폴더 선택', width=15,command=selectFo
 selectFolButton.place(x=0, y=20)
 
 
-#python을 이용해 키워드 입력시  텍스트나 소스파일에 있는 그 내용에서 입력한 키워드가 포함된 파일을 따로 모아준다.
-def get_KEYWORD_Files():
-    # 1. 키워드 입력 받기
-    keyword = input("Enter a keyword: ")
+import chardet
 
-    # 2. 모든 파일을 탐색하여 키워드가 포함된 파일의 경로를 찾기
-    found_files = []
-    for root, dirs, files in os.walk("."):
-        for filename in files:
-            filepath = os.path.join(root, filename)
-            with open(filepath, "r") as f:
-                file_content = f.read()
-                if keyword in file_content:
-                    found_files.append(filename)
+def fileReadTypes(filename):
+    ext = ['.txt', '.py', '.c', '.cpp', '.java', '.js', '.go', '.rb', '.rs', '.php']
+    return any(filename.endswith(ext) for ext in ext) #filename이 ext안에 해당하는 파일이면 true리턴
 
-    # 3. 찾은 파일 이름을 리스트에 저장하기
-    # 4. 저장된 파일 이름 출력하기
-    if found_files:
-        print("Found files:")
-        for filename in found_files:
-            print(filename)
+
+def getKeywordFiles(keyword):
+    global selectedFolerpath
+    if selectedFolerpath == '':
+        messagebox.showerror('에러', '폴더를 먼저 지정하십시오')
+        return
+    sorted_keyword_path = selectedFolerpath + '/' + keyword
+    if not os.path.exists(sorted_keyword_path):
+        os.makedirs(sorted_keyword_path)
+
+    for root, dirs, files in os.walk(selectedFolerpath):
+        for file in files:
+            if fileReadTypes(file):
+                file_path = os.path.join(root, file)
+                with open(file_path, 'rb') as f:
+                    binary_contents = f.read()
+                detected = chardet.detect(binary_contents)
+                encoding = detected['encoding']
+                contents = binary_contents.decode(encoding)
+
+                if keyword in contents:
+                    dest_dir = sorted_keyword_path
+                    if not os.path.exists(dest_dir):
+                        os.makedirs(dest_dir)
+                    dest_path = os.path.join(dest_dir, file)
+
+                    # 원본 파일과 대상 파일이 동일한지 확인하고 같으면 건너뜀
+                    if os.path.abspath(file_path) == os.path.abspath(dest_path):
+                        continue
+                    shutil.copy(file_path, dest_path)
+    messagebox.showinfo('성공', '입력한 키워드가 포함된 파일을 모아왔습니다.')
+    selectedFolerpath = ''
+    global pathlabel
+    pathlabel.configure(text='폴더 경로: ' + selectedFolerpath)  # 경로 출력을 위해 label 업데이트
+
+
+def getKEYWORD():
+    input_text = inputbox.get()
+    getKeywordFiles(input_text)
+
+
+getKeyword = Button(window, text='입력한 키워드 포함된파일 모으기', width=30, command=getKEYWORD)
+getKeyword.place(x=280, y=100)
+
+
+
+
+copied_ext = list()
+def copy_files_EXT(ext):
+
+    global selectedFolerpath
+    extflag=False
+    if selectedFolerpath == '':
+        messagebox.showerror('에러', '폴더를 먼저 지정하십시오')
+        return
+    sorted_files_folder = os.path.join(selectedFolerpath, '분류된 파일')
+    if not os.path.exists(sorted_files_folder):
+        os.makedirs(sorted_files_folder)
+
+    ext_folder = os.path.join(sorted_files_folder, ext)
+
+
+    for root, dirs, files in os.walk(selectedFolerpath):
+        for file in files:
+            if file.endswith(ext):
+                if not os.path.exists(ext_folder):
+                    os.makedirs(ext_folder)
+                    copied_ext.append(ext)
+                    extflag=True
+                if ext in copied_ext:
+                    extflag=True
+                abspath = os.path.join(root, file)
+                movedpath = os.path.join(ext_folder, file)
+                if abspath != movedpath:
+                    shutil.copy(abspath, movedpath)
+    if extflag==True:
+        messagebox.showinfo('성공', f'{ext} 파일끼리 분류가 완료되었습니다.')
     else:
-        print("No files found.")
-
-
-#특정 폴더 내에서 오늘 날짜를 기준으로 30일, 60일, 90일, 6개월 이상 사용하지 않은(액세스 하지 않은) 폴더 및 파일들에 대해서 따로 OLD 폴더로 옮겨주는 함수
-def move_old_files(folder_path):
-    # 1. 오늘 날짜 가져오기
-    today = datetime.datetime.today()
-
-    # 2. OLD 폴더 생성
-    old_folder = os.path.join(folder_path, "OLD")
-    if not os.path.exists(old_folder):
-        os.mkdir(old_folder)
-
-    # 3. 폴더 및 파일 탐색
-    moved_files = []
-    for root, dirs, files in os.walk(folder_path):
-        for d in dirs:
-            dir_path = os.path.join(root, d)
-            try:
-                accessed_time = os.path.getatime(dir_path)
-            except:
-                continue
-            accessed_date = datetime.datetime.fromtimestamp(accessed_time)
-            elapsed_days = (today - accessed_date).days
-            if elapsed_days >= 30 and elapsed_days < 60:
-                print(f"Moving {dir_path} to {old_folder}")
-                shutil.move(dir_path, old_folder)
-                moved_files.append(dir_path)
-            elif elapsed_days >= 60 and elapsed_days < 90:
-                print(f"Moving {dir_path} to {old_folder}")
-                shutil.move(dir_path, old_folder)
-                moved_files.append(dir_path)
-            elif elapsed_days >= 90 and elapsed_days < 180:
-                print(f"Moving {dir_path} to {old_folder}")
-                shutil.move(dir_path, old_folder)
-                moved_files.append(dir_path)
-            elif elapsed_days >= 180:
-                print(f"Moving {dir_path} to {old_folder}")
-                shutil.move(dir_path, old_folder)
-                moved_files.append(dir_path)
-
-        for f in files:
-            file_path = os.path.join(root, f)
-            try:
-                accessed_time = os.path.getatime(file_path)
-            except:
-                continue
-            accessed_date = datetime.datetime.fromtimestamp(accessed_time)
-            elapsed_days = (today - accessed_date).days
-            if elapsed_days >= 30 and elapsed_days < 60:
-                print(f"Moving {file_path} to {old_folder}")
-                shutil.move(file_path, old_folder)
-                moved_files.append(file_path)
-            elif elapsed_days >= 60 and elapsed_days < 90:
-                print(f"Moving {file_path} to {old_folder}")
-                shutil.move(file_path, old_folder)
-                moved_files.append(file_path)
-            elif elapsed_days >= 90 and elapsed_days < 180:
-                print(f"Moving {file_path} to {old_folder}")
-                shutil.move(file_path, old_folder)
-                moved_files.append(file_path)
-            elif elapsed_days >= 180:
-                print(f"Moving {file_path} to {old_folder}")
-                shutil.move(file_path, old_folder)
-                moved_files.append(file_path)
-
-    # 4. OLD 폴더로 이동한 파일 리스트를 이메일로 보내기
-    # send_email(moved_files)
-
-def send_email(moved_files):
-    pass
-    # # 1. 이메일 설정
-    # smtp_server = "smtp.gmail.com"  # SMTP 서버 주소
-    # smtp_port = 587  # SMTP 포트 번호
-    # sender_email = "sender_email_address"  # 발신자 이메일 주소
-    # sender_password = "sender_email_password"  # 발신자 이메일 비밀번호
-    # recipient_email = "recipient_email_address"  # 수신자 이메일 주소
-    #
-    # # 2. 이메일 내용 생성
-    # message = MIMEMultipart()
-    # message["From"] = sender_email
-    # message["To"] = recipient_email
-    # message["Subject"] = "Moved Files List"
-    # message.attach(MIMEText("\n".join(moved_files), "plain"))
-    #
-    # # 3. SMTP 서버에 로그인하여 이메일 보내기
-    # with smtplib.SMTP(smtp_server, smtp_port) as server:
-    #     server.starttls()
-    #     server.login(sender_email, sender_password)
-    #     server.sendmail(sender_email, recipient_email, message.as_string())
+        messagebox.showerror('실패', f'{ext} 파일이 없거나 분류에 실패했습니다.')
 
 
 
 
-getEXT = Button(window, text='입력한 확장자 모으기', width=30 , command=None)
-getEXT.place(x=0, y=100)
+#확장자 입력받기
+def gettext():
+    input_text = inputbox.get()
+    copy_files_EXT(input_text)
+
+inputbox = CTkEntry(window, width=100)
+inputbox.place(x=0, y=100)
+
+getEXT = Button(window, text='입력한 확장자 모으기', width=30, command=gettext)
+getEXT.place(x=130, y=100)
+
 
 
 #정보 및 도움말 팝업
 def showInfo():
-    messagebox.showinfo('도움말', '폴더 선택 후 사진 분류버튼을 누르세요.\n선택된 폴더 내의 사진들을 연월별로 분류합니다.\nTOPDF: 워드, PPT 파일을 PDF로 변환할 수 있습니다.\n오래된 파일 정리: 폴더 선택 후 오래된 기준을 선택하면 정리가 시작됩니다. \n\n 만든이: 한국공학대학교 게임공학과 강승호')
+    messagebox.showinfo('도움말', '폴더 선택 후 사진 분류버튼을 누르세요.\n선택된 폴더 내의 사진들을 연월별로 분류합니다.\nTOPDF: 워드, PPT 파일을 PDF로 변환할 수 있습니다.\n오래된 파일 정리: 폴더 선택 후 오래된 기준을 선택하면\nOLD.zip 파일로 모으는 정리가 시작됩니다.\n입력한 확장자 모으기: 폴더 선택 후 박스에 원하는 확장자를\n입력하면 입력한 확장자 이름의 폴더로 해당 확장자 파일들을 복사합니다. \n\n만든이: 2019182001 게임공학과 강승호')
 
 
 InfoButton = Button(window, text='도움말', width=15, command=showInfo)
-InfoButton.place(x=500, y=20)
-
-
-def stop(event=None):
-    window.quit()
+InfoButton.place(x=530, y=20)
 
 
 
-# from tkinter import *
-# menu = Menu()
-# menu_quit = Menu(menu, tearoff=False)
-# menu_quit.add_command(label="끝내기", accelerator='Ctrl+Q', command=stop)
+
 
 selectedFilePath = ''
 
@@ -248,30 +209,28 @@ def select_File():
         file_name = os.path.splitext(os.path.basename(selectedFilePath))[0]
         output_dir = os.path.dirname(selectedFilePath)
         output_file = os.path.join(output_dir, file_name + '.pdf')
-        convert_to_pdf(selectedFilePath, output_file)
+        convert2PDF(selectedFilePath, output_file)
 
 
-import comtypes.client
-
-def convert_to_pdf(input_file, output_file):
+def convert2PDF(input_file, output_file):
     # MS Office 어플리케이션 생성
     powerpoint = comtypes.client.CreateObject('Powerpoint.Application')
     word = comtypes.client.CreateObject('Word.Application')
 
-    input_file = os.fsdecode(input_file)  # 파일 경로에 있는 Unicode 문자를 처리합니다.
-    output_file = os.fsdecode(output_file)  # 파일 경로에 있는 Unicode 문자를 처리합니다.
+    input_file = os.fsdecode(input_file)  # 파일 경로에 있는 유니코드문자처리
+    output_file = os.fsdecode(output_file)  # 파일 경로에 있는 유니코드문자처리
 
     try:
-        # PPT 파일을 PDF로 변환
+        # PPT -> PDF
         if input_file.endswith('.ppt') or input_file.endswith('.pptx'):
             presentation = powerpoint.Presentations.Open(input_file)
-            presentation.SaveAs(output_file, FileFormat=32)  # ppSaveAsPDF 상수 값 (32)를 직접 사용합니다.
+            presentation.SaveAs(output_file, FileFormat=32)  # 파워포인트를 pdf로 저장하는 상수값 32
             presentation.Close()
 
-        # Word 파일을 PDF로 변환
+        # Word -> PDF
         elif input_file.endswith('.doc') or input_file.endswith('.docx'):
             document = word.Documents.Open(input_file)
-            document.SaveAs(output_file, FileFormat=17)  # wdFormatPDF 상수 값 (17)를 직접 사용합니다.
+            document.SaveAs(output_file, FileFormat=17)  # 워드를 pdf로 저장하는 상수값 17
             document.Close()
 
         # 지원하지 않는 파일 형식인 경우
@@ -300,7 +259,7 @@ days.set(0)
 
 
 
-def zip_and_remove_old_folder():
+def zip_and_remove_OLD():
     global selectedFolerpath
     old_folder = os.path.join(selectedFolerpath, 'OLD')
     zip_file = os.path.join(selectedFolerpath, 'OLD.zip')
@@ -333,7 +292,7 @@ def move_old_files(days_threshold):
             if file_age.days > days_threshold.get():  # get() 메서드를 사용하여 IntVar 값으로 가져옴 (int랑 intVar이랑 비교가 안되기 때문에 수정함)
                 shutil.move(file_path, os.path.join(old_folder, file))
 
-    zip_and_remove_old_folder()
+    zip_and_remove_OLD()
 
 
 def select_gijun_Date_popup():
@@ -346,23 +305,19 @@ def select_gijun_Date_popup():
     popup.title('기준 기간 선택창')
     popup.geometry('300x240+700+400')
     popup.transient(window)
-    # option = IntVar(value=0)
 
     option1_radiobutton = Radiobutton(popup, text="7 days", variable=days, value=7)
     option2_radiobutton = Radiobutton(popup, text="14 days", variable=days, value=14)
     option3_radiobutton = Radiobutton(popup, text="30 days", variable=days, value=30)
     option4_radiobutton = Radiobutton(popup, text="90 days", variable=days, value=90)
     option5_radiobutton = Radiobutton(popup, text="180 days", variable=days, value=180)
-
-
-
     option1_radiobutton.pack()
     option2_radiobutton.pack()
     option3_radiobutton.pack()
     option4_radiobutton.pack()
     option5_radiobutton.pack()
 
-    def on_confirm():
+    def date_confirm():
         if days.get() == 7:
             messagebox.showinfo('확인', '7일을 선택하셨습니다.\n열어본지 7일 이상된 파일들을 정리합니다.')
             move_old_files(days)
@@ -382,14 +337,14 @@ def select_gijun_Date_popup():
             messagebox.showinfo('선택하세요', '옵션을 선택해주세요.')
         popup.destroy()
 
-    confirm_button = Button(popup, text='기간 선택', command=on_confirm)
+    confirm_button = Button(popup, text='기간 선택', command=date_confirm)
     confirm_button.pack(side=BOTTOM)
 
 select_gijun_Date_Button = Button(window, text='오래된 파일 모으기: 기간 선택하기', command=select_gijun_Date_popup)
 select_gijun_Date_Button.place(x=170, y=20)
 
-
-# window.config(menu=menu)
+def stop(event=None):
+    window.quit()
 
 window.bind('<Escape>', stop)
 window.mainloop()
